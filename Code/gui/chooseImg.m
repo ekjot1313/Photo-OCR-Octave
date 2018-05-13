18260f28c196cee8c74dec3ba174d90445060802
ekjot1313 <ekjotsingh00776@gmail.com>
2018-05-13 19:22:10 +0530

    Add files via upload

--- Code/gui/chooseImg.m
+++ Code/gui/chooseImg.m
@@ -1,15 +1,18 @@
 function chooseImg(source,event,slectedImgEdit)
+% this function helps to select image
 
-[imgname, imgpath]=uigetfile ({"*.jpeg;*.png;*.jpg", "Supported Picture Formats"},'Select Image');
-%i=imread('E:\Photo OCR\Project\Code\Sample Images\abcd1.jpg'); % reads given image in rgb form
-imgpath=strcat(imgpath,imgname);
 
-set(slectedImgEdit,'string',imgpath);
+[imgname, imgpath]=uigetfile ({"*.jpeg;*.png;*.jpg",...
+                             "Supported Picture Formats"},'Select Image');
+% open a file chooser and return image path and image name
 
-i=imread(imgpath); % reads given image in rgb form
-figure;
-imshow(i);
+absimgpath=strcat(imgpath,imgname);
+% absolute image path
 
-addpath('..\');
-%stage1(i);
+set(slectedImgEdit,'string',absimgpath);
+% update slectedImgEdit with the absolute image path
+
+i=imread(absimgpath);
+% reads given image in rgb form
+
 end;
